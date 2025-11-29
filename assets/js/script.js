5ad95c7206e5023dbe29cb11b379402518c0839f
pratikshagkumbhar <pratikshaganeshkumbhar>
2025-11-29 13:58:10 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -189,7 +189,7 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
+/*// Start of Tawk.to Live Chat
 var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
@@ -199,7 +199,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
 })();
-// End of Tawk.to Live Chat
+// End of Tawk.to Live Chat*/
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
